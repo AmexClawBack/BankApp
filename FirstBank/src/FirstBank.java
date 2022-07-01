ca0f68514b572630bde86f1eed3224f79456840d
dillo <Dilloncking@gmail.com>
2022-06-30 19:55:08 -0500

    First Commit

---
+++ FirstBank/src/FirstBank.java
@@ -0,0 +1,10 @@
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account dillon  = new Account("Dillon King", "A00001");
+		dillon.showMenu();
+	}
+
+}
